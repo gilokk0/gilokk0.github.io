f47aa75af6d0ae55aa855b60d5af2b00d81de001
Gilokk0 <17620548+gilokk0@users.noreply.github.com>
2020-03-10 02:11:22 +0000

    Update

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
+var arrayOfFilesToCache = [
   '/',
   '/index.md',
   '/404.md',
@@ -54,50 +53,38 @@ var urlsToCache = [
   '/assets/images/site-2.png',
   '/assets/images/small-ribbon-tail-sprite.png',
   '/assets/images/small-ribbon-tail-sprite-2x.png'
-];
+]
 
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(currentCacheName).then(function(cache) {
+        return cache.addAll(arrayOfFilesToCache);
       })
     );
+  });
+
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      // Get all the cache names
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          // Get all the items that are stored under a different cache name than the current one
+          cacheNames.filter(function(cacheName) {
+            return cacheName != currentCacheName;
+          }).map(function(cacheName) {
+            // Delete the items
+            return caches.delete(cacheName);
+          })
+        ); // end Promise.all()
+      }) // end caches.keys()
+    ); // end event.waitUntil()
+  });
+
+  self.addEventListener('fetch', function(event) {
+  // Do stuff with fetch events
+});
+
+self.addEventListener('message', function(event) {
+  // Do stuff with postMessages received from document
 });
